7bd5f1618b7fa7ba061cc9b73d49c7df769a9370
RNewsom <ryanjnewsom@gmail.com>
2016-11-21 20:12:13 -0700

    Added fix for initial nav drawer launch

--- app/src/main/java/ryannewsom/com/castlerockdental/MainActivity.java
+++ app/src/main/java/ryannewsom/com/castlerockdental/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        navigationView.setCheckedItem(R.id.nav_schedule);
+
         launchScheduleFragment();
 
 
