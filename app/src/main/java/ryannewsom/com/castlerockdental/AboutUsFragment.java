e70ba346d0b2e628fd2481253e2612f55442a40c
RNewsom <ryanjnewsom@gmail.com>
2016-11-25 17:14:45 -0700

    fixed about us

--- app/src/main/java/ryannewsom/com/castlerockdental/AboutUsFragment.java
+++ app/src/main/java/ryannewsom/com/castlerockdental/AboutUsFragment.java
@@ -26,6 +26,7 @@ public class AboutUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        getActivity().setTitle("About Us");
         return inflater.inflate(R.layout.fragment_about_us, container, false);
     }
 
